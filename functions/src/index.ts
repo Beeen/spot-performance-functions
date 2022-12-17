272630c1a982df70c73aeb1112030b20eb5044dc
Benjamin Ledoux <benjaminledoux@Benjamins-MacBook-Pro.local>
2022-12-17 16:35:17 +0000

    Following tutorial

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,16 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-//const functions = require('firebase-functions');
+//The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+//  const functions = require('firebase-functions');
 
-// The Firebase Admin SDK to access Firestore.
-//const admin = require('firebase-admin');
+//  The Firebase Admin SDK to access Firestore.
+//  const admin = require('firebase-admin');
 
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 admin.initializeApp();
+const { Logging } = require('@google-cloud/logging');
+const logging = new Logging({
+  projectId: process.env.GCLOUD_PROJECT,
+});
 
 const express = require("express");
 const app = express();
@@ -53,30 +57,195 @@ exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
       return snap.ref.set({uppercase}, {merge: true});
     });
 
-app.post('/payment-sheet', async (req, res) => {
-    // Use an existing Customer ID if this is a returning customer.
-    const customer = await stripe.customers.create();
-    const ephemeralKey = await stripe.ephemeralKeys.create(
-    {customer: customer.id},
-    {apiVersion: '2020-08-27'}
-    );
-    const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1099,
-    currency: 'eur',
-    customer: customer.id,
-    automatic_payment_methods: {
-        enabled: true,
-    },
+/**
+ * When a user is created, create a Stripe customer object for them.
+ *
+ * @see https://stripe.com/docs/payments/save-and-reuse#web-create-customer
+ */
+ exports.createStripeCustomer = functions.auth.user().onCreate(async (user) => {
+    const customer = await stripe.customers.create({ email: user.email });
+    const intent = await stripe.setupIntents.create({
+      customer: customer.id,
     });
-
-    res.json({
-    paymentIntent: paymentIntent.client_secret,
-    ephemeralKey: ephemeralKey.secret,
-    customer: customer.id,
-    publishableKey: 'pk_test_51JWmMXAqPOYdz9ujfTkJ4VNx48TJX3Gtg1m8Pk41qNFMteqOepRHCOUMZQaTL00JZixm9HBTx7gIAzcb8U0PixDA00Hci4fj1w'
+    await admin.firestore().collection('stripe_customers').doc(user.uid).set({
+      customer_id: customer.id,
+      setup_secret: intent.client_secret,
     });
-});
+    return;
+  });
+  
+  /**
+ * When adding the payment method ID on the client,
+ * this function is triggered to retrieve the payment method details.
+ */
+  exports.addPaymentMethodDetails = functions.firestore
+    .document('/stripe_customers/{userId}/payment_methods/{pushId}')
+    .onCreate(async (snap, context) => {
+      try {
+        const paymentMethodId = snap.data().id;
+        const paymentMethod = await stripe.paymentMethods.retrieve(
+          paymentMethodId
+        );
 
-app.listen(process.env.PORT, () =>
-  console.log(`Node server listening on port ${process.env.PORT}!`)
-);
+        await snap.ref.set(paymentMethod);
+        // Create a new SetupIntent so the customer can add a new method next time.
+        const intent = await stripe.setupIntents.create({
+          customer: `${paymentMethod.customer}`,
+        });
+
+        await snap.ref.parent.parent!.set(
+          {
+            setup_secret: intent.client_secret,
+          },
+          { merge: true }
+        );
+
+        return;
+      } catch (error) {
+        await snap.ref.set({ error: userFacingMessage(error) }, { merge: true });
+        await reportError(error, { user: context.params.userId });
+      }
+    });
+  
+  /**
+   * When a payment document is written on the client,
+   * this function is triggered to create the payment in Stripe.
+   *
+   * @see https://stripe.com/docs/payments/save-and-reuse#web-create-payment-intent-off-session
+   */
+  
+  // [START chargecustomer]
+  
+  exports.createStripePayment = functions.firestore
+    .document('stripe_customers/{userId}/payments/{pushId}')
+    .onCreate(async (snap, context) => {
+      const { amount, currency, payment_method } = snap.data();
+      try {
+        // Look up the Stripe customer id.
+        const parent_ref = await snap!.ref!.parent!.parent!.get()
+        const customer = parent_ref.data()!.customer_id;
+        // Create a charge using the pushId as the idempotency key
+        // to protect against double charges.
+        const idempotencyKey = context.params.pushId;
+        const payment = await stripe.paymentIntents.create(
+          {
+            amount,
+            currency,
+            customer,
+            payment_method,
+            off_session: false,
+            confirm: true,
+            confirmation_method: 'manual',
+          },
+          { idempotencyKey }
+        );
+        // If the result is successful, write it back to the database.
+        await snap.ref.set(payment);
+      } catch (error) {
+        // We want to capture errors and render them in a user-friendly way, while
+        // still logging an exception to Error Reporting.
+        functions.logger.log(error);
+        await snap.ref.set({ error: userFacingMessage(error) }, { merge: true });
+        await reportError(error, { user: context.params.userId });
+      }
+    });
+  
+  // [END chargecustomer]
+  
+  /**
+   * When 3D Secure is performed, we need to reconfirm the payment
+   * after authentication has been performed.
+   *
+   * @see https://stripe.com/docs/payments/accept-a-payment-synchronously#web-confirm-payment
+   */
+  exports.confirmStripePayment = functions.firestore
+    .document('stripe_customers/{userId}/payments/{pushId}')
+    .onUpdate(async (change, context) => {
+      if (change.after.data().status === 'requires_confirmation') {
+        const payment = await stripe.paymentIntents.confirm(
+          change.after.data().id
+        );
+        change.after.ref.set(payment);
+      }
+    });
+  
+  /**
+   * When a user deletes their account, clean up after them
+   */
+  exports.cleanupUser = functions.auth.user().onDelete(async (user) => {
+    const dbRef = admin.firestore().collection('stripe_customers');
+    const customer = (await dbRef.doc(user.uid).get()).data();
+    await stripe.customers.del(customer!.customer_id);
+    // Delete the customers payments & payment methods in firestore.
+    const batch = admin.firestore().batch();
+    const paymetsMethodsSnapshot = await dbRef
+      .doc(user.uid)
+      .collection('payment_methods')
+      .get();
+    paymetsMethodsSnapshot.forEach((snap) => batch.delete(snap.ref));
+    const paymentsSnapshot = await dbRef
+      .doc(user.uid)
+      .collection('payments')
+      .get();
+    paymentsSnapshot.forEach((snap) => batch.delete(snap.ref));
+  
+    await batch.commit();
+  
+    await dbRef.doc(user.uid).delete();
+    return;
+  });
+  
+  /**
+   * To keep on top of errors, we should raise a verbose error report with Error Reporting rather
+   * than simply relying on functions.logger.error. This will calculate users affected + send you email
+   * alerts, if you've opted into receiving them.
+   */
+  
+  // [START reporterror]
+  
+  function reportError(err: any, context = {}) {
+    // This is the name of the log stream that will receive the log
+    // entry. This name can be any valid log stream name, but must contain "err"
+    // in order for the error to be picked up by Error Reporting.
+    const logName = 'errors';
+    const log = logging.log(logName);
+  
+    // https://cloud.google.com/logging/docs/api/ref_v2beta1/rest/v2beta1/MonitoredResource
+    const metadata = {
+      resource: {
+        type: 'cloud_function',
+        labels: { function_name: process.env.FUNCTION_NAME },
+      },
+    };
+  
+    // https://cloud.google.com/error-reporting/reference/rest/v1beta1/ErrorEvent
+    const errorEvent = {
+      message: err.stack,
+      serviceContext: {
+        service: process.env.FUNCTION_NAME,
+        resourceType: 'cloud_function',
+      },
+      context: context,
+    };
+  
+    // Write the error log entry
+    return new Promise<void>((resolve, reject) => {
+      log.write(log.entry(metadata, errorEvent), (error: any) => {
+        if (error) {
+          return reject(error);
+        }
+        return resolve();
+      });
+    });
+  }
+  
+  // [END reporterror]
+  
+  /**
+   * Sanitize the error message for the user.
+   */
+  function userFacingMessage(error: any) {
+    return error.type
+      ? error.message
+      : 'An error occurred, developers have been alerted';
+  }
